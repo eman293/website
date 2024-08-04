2ff4c203bb26e3927d3fb5dd0fd7a8419eb49dcf
eman293 <116614867+eman293@users.noreply.github.com>
2024-08-03 22:46:06 -0500

    test 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 // To see updates on github pages:
 // commit + push to Master branch 
 // type "npm run deploy" in terminal
+//takes a while - don't be impatient
 
 function App() {
   return (
