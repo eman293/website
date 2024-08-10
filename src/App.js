eb8c9c71e0fafe95f2e60e5a3f99041b5808f7ea
eman293 <116614867+eman293@users.noreply.github.com>
2024-08-09 22:40:04 -0500

    blank website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 // To see updates on github pages:
@@ -10,18 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Did it work?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
